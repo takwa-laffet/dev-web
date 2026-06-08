88764c9e02ffa412ad06851069bcd42190c294bf
takwa-laffet <laffet@kebili.r-iset.tn>
2026-06-08 21:00:23 +0100

    s13

---
+++ bases-de-donnes/db.py
@@ -0,0 +1,73 @@
+import mysql.connector
+c=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="testdb"
+    )
+cursor=c.cursor()
+#show databases
+cursor.execute("show databases")
+for db in cursor:
+    print(db)
+
+#create table
+""" cursor.execute("create table etudiant1(id integer,nom varchar(100),age integer)")
+c.commit()
+ """
+# create table
+""" cursor.execute("create table enseignant(id integer auto_increment primary key," \
+"nom varchar(100),age integer)") """
+""" 
+c.commit() """
+#insert donner
+""" 
+cursor.execute("insert enseignant(nom,age)values('maryam',23)") """
+""" cursor.execute("insert enseignant(nom,age)values('Hafiza',20)")
+ """
+""" name=input("enter name")
+age=int(input("enter age"))
+cursor.execute('insert into enseignant(nom,age) values(%s,%s)',(name,age))
+c.commit() """
+#select data
+""" cursor.execute("select * from enseignant")
+for row in cursor:
+    print(row)
+cursor.execute("select nom from enseignant")
+for row in cursor:
+    print(row)
+cursor.execute("select * from enseignant where age>18")
+for row in cursor:
+    print(row)
+ """
+#update data
+""" cursor.execute("update enseignant set nom='ali'where id=4")
+c.commit()
+ """
+""" #delete data
+cursor.execute("delete from enseignant where id=4")
+c.commit()
+ """
+""" cursor.execute("select * from enseignant")
+r= cursor.fetchall()
+print(r[0])
+for row in r:
+    print(row[1])
+
+ """""" 
+cursor.execute("drop table etudiant")
+c.commit() """
+#alter table 
+""" cursor.execute("alter table etudiant1 add column email varchar(100)" ) """
+""" cursor.execute("alter table etudiant1 rename to etudiant" )
+ """
+""" cursor.execute("alter table etudiant change column email classe varchar(255)")
+ """
+""" cursor.execute("alter table etudiant drop column classe")
+ """
+#database
+""" cursor.execute("create database if not exists db1")
+ """
+""" cursor.execute("drop database db1")
+ """
+c.close()
